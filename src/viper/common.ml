c4e9eb0a26e0089bf28a13698b1f03b11b6bfa8b
Vladislav Zavialov <vlad.z.4096@gmail.com>
2024-06-09 17:48:03 +0200

    motoko-san: ADT encoding for tuples

--- src/viper/common.ml
+++ src/viper/common.ml
@@ -8,3 +8,14 @@ let rec map_last ~f = function
   | [] -> []
   | [ x ] -> [ f x ]
   | x :: xs -> x :: map_last ~f xs
+
+let tup_con_name n =
+  Printf.sprintf "Tup$%d" n
+
+let tup_prj_name n i =
+  Printf.sprintf "tup$%d$%d" n i
+
+module IntSet = Set.Make(struct
+  type t = int
+  let compare = compare
+end)
